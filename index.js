296c759aaaea6090d900602955590a0fcec2b594
MirwaisAzeem10 <mirwais.azeem1@gmail.com>
2022-01-28 20:53:18 +0500

    Drenc On Process

---
+++ index.js
@@ -0,0 +1,10 @@
+const sideBarSmallScreen = document.querySelector('.small__side_bar')
+const sideBarSmallScreenBtn = document.querySelector('.sidebar-toggle__button')
+
+sideBarSmallScreen.addEventListener('click', () => sideBarSmallScreen.classList.toggle('hide'))
+
+
+{/* <div class="relative">
+    text
+    <hr style="position:absolute;left:0"/>
+</div> */}
